8890b0077a6752b72bd6d4374280129b56cab192
Fordandra <cassie@coreyford.com>
2018-11-02 11:01:12 -0500

    update commit
    Replaced teacher's abduction files with personal in-class files

---
+++ js/abduction.js
@@ -0,0 +1,53 @@
+var form = document.getElementsByTagName("form")[0];
+
+form.addEventListener("submit", function(e) {
+
+  e.preventDefault();
+
+  sendData();
+
+});
+
+
+
+// https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
+
+function sendData() {
+
+  var XHR = new XMLHttpRequest();
+
+  var urlEncodedData = "";
+
+  var urlEncodedDataPairs = [];
+
+
+
+  urlEncodedDataPairs.push(
+
+    encodeURIComponent("name") +
+
+      "=" +
+
+      encodeURIComponent(form.querySelector("[name='name']").value)
+
+  );
+
+  urlEncodedDataPairs.push(
+
+    encodeURIComponent("send_to") +
+
+      "=" +
+
+      encodeURIComponent(form.querySelector("[name='send_to']").value)
+
+  );
+
+  urlEncodedDataPairs.push(
+
+    encodeURIComponent("email") +
+
+      "=" +
+
+      encodeURIComponent(form.querySelector("[name='email']").value)
+
+  );
